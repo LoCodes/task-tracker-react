f994e0c6aa36aacc73a4157a1f294d27ebeb1e50
Loren Cipriano <loren25cipriano@gmail.com>
2022-03-14 20:37:00 -0700

    delete files, practice const variable and ternary

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 function App() {
+  const name = 'Loren'
+  const x = false
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+     <h1>Hello From {name}</h1>
+     <h2> ternary {x ? 'yes' : 'no' } </h2>
+
     </div>
   );
 }
