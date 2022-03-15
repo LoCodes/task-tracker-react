3500014b22e0b27704a7371df98950d01f66d7fe
Loren Cipriano <loren25cipriano@gmail.com>
2022-03-14 21:18:57 -0700

    styling with JS

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,20 +3,26 @@ import PropTypes from 'prop-types'
 
 const Header = ({ title }) => {
   return (
-    <div> 
-       <h1>{title}</h1>
-    </div>
+    <header> 
+       <h1 style={headingStyle} >{title}</h1>
+    </header>
   )
 }
 
 // default props if there is no prop passed in the Header component in app.js 
 Header.defaultProps = {
-    title: 'Task Tracker'
+    title: 'Task Tracker',
 }
 
 // great for catching errors, making code more robust
-Header.PropTypes = { 
-    title: PropTypes.string.isRequired
+Header.propTypes = { 
+    title: PropTypes.string
+}
+
+// CSS in JS 
+const headingStyle = {
+    color: 'red', 
+    backgroundColor: 'black'
 }
 
 export default Header
