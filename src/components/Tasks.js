afa4921487840d43d0ffcc667531e2780297316a
Loren Cipriano <loren25cipriano@gmail.com>
2022-03-15 15:25:11 -0700

    create tasks component

---
+++ src/components/Tasks.js
@@ -0,0 +1,31 @@
+const tasks = [
+    {
+        id: 1, 
+        text: 'Interview with Tomorrow Health',
+        dateTime: 'March 15th, 2022 at 1:40pm',
+        reminder: true
+
+    },
+    {
+        id: 2, 
+        text: 'Get off at 6pm or later',
+        dateTime: 'March 15th, 2022',
+        reminder: false
+
+    },
+    {
+        id: 3, 
+        text: 'This is dummy data',
+        dateTime: 'March 15th, 2022 at 3:21pm',
+        reminder: true
+
+    }
+]
+
+const Tasks = () => {
+  return (
+    <div>Tasks</div>
+  )
+}
+
+export default Tasks
