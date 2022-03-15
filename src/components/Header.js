10e0ce41fb9ac9d2f22a412f2c4de6e54d012247
Loren Cipriano <loren25cipriano@gmail.com>
2022-03-15 12:11:35 -0700

    create button component, and add props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 // import React from 'react'
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({ title }) => {
   return (
     <header className='header'> 
        <h1>{title}</h1>
-       <button className='btn'> Add </button>
-    </header>
+       {/* reusable component */}
+       <Button color='green' text='Add' /> 
+       
+    </header> 
   ) 
 }
 
