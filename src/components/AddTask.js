0dfc9035d7c1e4caeab8cf145793880847f4c50d
Loren Cipriano <loren25cipriano@gmail.com>
2022-03-17 07:11:19 -0700

    create form input state(controlled component)

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,19 +1,29 @@
-// import React from 'react'
+import { useState } from "react"
 
 const AddTask = () => {
+    // component level state 
+    const [text, setText] = useState('')
+    const [dateTime, setDateTime] = useState('')
+    const [reminder, setReminder] = useState(false)
+
+    //onChange on controlled component 
+
   return (
     <form className='add-form'>
         <div className='form-control'> 
             <label> Task </label>
-            <input type='text' placeholder='Add Task' /> 
+            <input type='text' placeholder='Add Task' value={text} 
+            onChange={(e)=>setText(e.target.value)}/> 
         </div>
         <div className='form-control'> 
             <label> Day & Time </label>
-            <input type='text' placeholder='Add Day & Time' /> 
+            <input type='text' placeholder='Add Day & Time' value={dateTime} 
+            onChange={(e)=>setDateTime(e.target.value)}/> 
         </div>
         <div className='form-control form-control-check' > 
             <label> Set Reminder </label>
-            <input type='checkbox' />
+            <input type='checkbox' value={reminder} 
+            onChange={(e)=>setReminder(e.currentTarget.checked)}/>
         </div>
 
         <input type='submit' value='Save Task' 
