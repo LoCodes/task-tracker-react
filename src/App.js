a335ad6be88b9111dc78358318877c38a2dd2c21
Loren Cipriano <loren25cipriano@gmail.com>
2022-03-21 20:54:12 -0700

    added About component in App.js and utilized Router, Route, Routes from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-// import React from "react";
 import { useState, useEffect   } from "react";
+import { BrowserRouter as Router, Route, Routes  } from "react-router-dom"; 
 import Header from "./components/Header";
 import { Footer } from "./components/Footer";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
+import About from "./components/About";
+
 
 
 function App() {
@@ -92,18 +94,31 @@ function App() {
 
     // setTasks([...tasks], {})
             // spread then add 
-
+    <Router> 
     <div className='container'>
      <Header 
      onAdd={()=> setShowAddTask(!showAddTask)} 
      showAdd={showAddTask}
      />
-    { showAddTask && <AddTask onAdd={addTask}/> }
-     
-     {tasks.length > 0 ? 
-     <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks'}
+     <Routes> 
+      <Route 
+        path='/' 
+        element={
+        <>
+        { showAddTask && <AddTask onAdd={addTask}/> }
+        {tasks.length > 0 ? 
+          <Tasks 
+            tasks={tasks} 
+            onDelete={deleteTask} 
+            onToggle={toggleReminder}/> : 'No Tasks'}      
+          </>
+      } 
+      />
+     <Route path="about" element={<About/>} />
+     </Routes>
      <Footer />
     </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,22 @@
 // import React from 'react'
 import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom' // lets us look at the route we're currently on 
 import Button from './Button'
 
 const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation()
+
   return (
     <header className='header'> 
        <h1>{title}</h1>
        {/* reusable component */}
-       <Button 
+       {location.pathname === '/' && 
+       (<Button 
        color={showAdd ? 'red' : 'green'}
        text={showAdd ? 'Close' : 'Add'} 
        onClick={onAdd}
-       /> 
+       />
+       )}
        
     </header> 
   ) 
